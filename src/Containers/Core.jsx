25d7487dbd8ba3931979494fde2fb7facc6f6ada
Mohammad Mattar <Mattarmoe1@gmail.com>
2019-04-22 15:34:12 -0400

    update routing

--- src/Containers/Core.jsx
+++ src/Containers/Core.jsx
@@ -5,7 +5,9 @@ import Header from '../Navigation/Header.jsx';
 import About from '../components/About.jsx';
 import HomeLayout from '../components/HomeLayout.jsx';
 import { Route } from 'react-router-dom';
-import DisplayAddPersons from '../components/DisplayAddPersons.jsx';
+import ItemListView from '../components/ItemListView.jsx';
+import CreateRequestForm from '../components/CreateRequestForm.jsx';
+import BecomeDriverForm from '../components/BecomeDriverForm.jsx';
 
 class Core extends Component {
   render() {
@@ -17,7 +19,9 @@ class Core extends Component {
         <Route path="/login-form" component={LoginForm} />
         <Route path="/signup-form" component={SignupForm} />
         <Route path="/about" component={About} />
-        <Route path="/api" component={DisplayAddPersons} />
+        <Route path="/list-view" component={ItemListView} />
+        <Route path="/createrequest-form" component={CreateRequestForm} />
+        <Route path="/becomedriver-form" component={BecomeDriverForm} />
       </div>
     );
   }
