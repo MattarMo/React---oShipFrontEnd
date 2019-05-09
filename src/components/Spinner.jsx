0778650a555ef732c47fa55e04fc71782d806544
Mohammad Mattar <Mattarmoe1@gmail.com>
2019-05-09 10:36:28 -0400

    Spinner component and Css

--- src/components/Spinner.jsx
+++ src/components/Spinner.jsx
@@ -1,8 +1,8 @@
 import React from 'react';
-import classes from '../styles/spinner.css';
+import '../styles/spinner.css';
 
 const Spinner = () => {
-  return <div className={classes.Loader}>Loading....</div>;
+  return <div className="loader">Loading....</div>;
 };
 
 export default Spinner;
